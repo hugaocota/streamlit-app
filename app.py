7a56eb51788e088b4b0d3d5ed3d6ea8e9409f2bf
hugaocota <142175172+hugaocota@users.noreply.github.com>
2024-09-04 13:48:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -66,137 +66,102 @@ else:
         st.error(f"Erro ao ler o arquivo Excel: {e}")
 
     # Menu lateral com opções principais
-    st.sidebar.title("Menu")
-    menu_option = st.sidebar.radio("Selecione uma opção:", [
-                                   "Script de Venda", "Máquinas", "Marcas", "Venda mais", "Segmentos"])
+st.sidebar.title("Menu")
+menu_option = st.sidebar.radio("Selecione uma opção:", [
+                               "Script de Venda", "Máquinas", "Marcas", "Venda mais", "Segmentos"])
 
-    # Opção 1: Script de Venda
-    if menu_option == "Script de Venda":
-        st.title("Simulação de Interação de Venda")
+# Opção 1: Script de Venda
+if menu_option == "Script de Venda":
+    st.title("Simulação de Interação de Venda")
 
-        # Apresentação do vendedor
-        st.subheader("Apresentação do Vendedor")
-        vendedor_nome = st.text_input("Seu Nome")
-        saudacao = st.radio("Escolha uma saudação:", [
-                            "Bom dia", "Boa tarde", "Boa noite"])
+    # Apresentação do vendedor
+    st.subheader("Apresentação do Vendedor")
+    vendedor_nome = st.text_input("Seu Nome")
+    saudacao = st.radio("Escolha uma saudação:", [
+                        "Bom dia", "Boa tarde", "Boa noite"])
 
-        # Saudação e pergunta inicial
-        cliente_nome = st.text_input("Nome do Cliente")
-        cliente_empresa = st.text_input("Empresa do Cliente")
+    # Saudação e pergunta inicial
+    cliente_nome = st.text_input("Nome do Cliente")
+    cliente_empresa = st.text_input("Empresa do Cliente")
 
-        if textos_dict:
-            # Substituir os placeholders nos textos do script
-            texto_saudacao = textos_dict.get('saudacao', 'Texto padrão de saudação')
-            texto_saudacao = texto_saudacao.replace("{cliente_nome}", cliente_nome).replace("{vendedor_nome}", vendedor_nome)
+    if textos_dict:
+        # Substituir os placeholders nos textos do script
+        texto_saudacao = textos_dict.get('saudacao', 'Texto padrão de saudação')
+        texto_saudacao = texto_saudacao.replace(
+            "{cliente_nome}", cliente_nome).replace("{vendedor_nome}", vendedor_nome)
 
-            texto_introducao = textos_dict.get('introducao', 'Texto padrão de introdução')
-            texto_ramo = textos_dict.get('pergunta_ramo', 'Texto padrão de pergunta sobre ramo')
-            texto_maquina = textos_dict.get('pergunta_maquina', 'Texto padrão de pergunta sobre máquina')
+        texto_introducao = textos_dict.get(
+            'introducao', 'Texto padrão de introdução')
+        texto_ramo = textos_dict.get(
+            'pergunta_ramo', 'Texto padrão de pergunta sobre ramo')
+        texto_maquina = textos_dict.get(
+            'pergunta_maquina', 'Texto padrão de pergunta sobre máquina')
 
-            # Exibir os textos no script
-            st.write(texto_saudacao)
-            st.write(texto_introducao)
-            
-            ramo_atuacao = st.text_input("Ramo de Atuação")
-            st.write(texto_ramo)
+        # Exibir os textos no script
+        st.write(texto_saudacao)
+        st.write(texto_introducao)
 
-            maquina_cliente = st.selectbox("Selecione a Máquina:", abas, index=abas.index(default_machine))
-            st.write(texto_maquina)
+        ramo_atuacao = st.text_input("Ramo de Atuação")
+        st.write(texto_ramo)
 
-            # Botão para carregar a imagem da máquina
-            if st.button("Mostrar Imagem da Máquina"):
-                carregar_imagem_maquina(maquina_cliente)
+        maquina_cliente = st.selectbox(
+            "Selecione a Máquina:", abas, index=abas.index(default_machine))
+        st.write(texto_maquina)
 
-        # Se uma máquina foi selecionada
-        if maquina_cliente:
+        # Botão para carregar a imagem da máquina
+        if st.button("Mostrar Imagem da Máquina"):
+            carregar_imagem_maquina(maquina_cliente)
+
+# Opção 2: Máquinas
+elif menu_option == "Máquinas":
+    st.title("Máquinas")
+
+    if abas:
+        maquinas_filtradas = abas
+        maquina_selecionada = st.selectbox(
+            "Selecione a Máquina:", maquinas_filtradas)
+
+        if maquina_selecionada:
             try:
-                df_maquina = pd.read_excel(xls, sheet_name=maquina_cliente)
+                df_maquina = pd.read_excel(
+                    xls, sheet_name=maquina_selecionada)
 
                 df_maquina = df_maquina.dropna(
                     how='all').loc[:, ~df_maquina.columns.str.contains('^Unnamed')]
 
-                st.write(f"Ótimo! Trabalhar com {maquina_cliente} é sempre uma escolha sólida. Agora, vamos ver como podemos ajudar a manter sua máquina em perfeitas condições.")
+                st.title(f"Dados da Máquina: {maquina_selecionada}")
+                st.dataframe(df_maquina, use_container_width=True)
 
-                coluna_nome = "DESCRIÇÃO/ KOMATSU D50"
-                if coluna_nome in df_maquina.columns:
-                    itens_lista = df_maquina[coluna_nome].dropna(
-                    ).unique().tolist()
-                    item_pesquisado = st.selectbox(
-                        "Pesquise o item desejado:", [""] + itens_lista)
-
-                    if item_pesquisado:
-                        itens_filtrados = df_maquina[df_maquina[coluna_nome]
-                                                     == item_pesquisado]
-                        if not itens_filtrados.empty:
-                            st.write(f"Você selecionou o item '{item_pesquisado}'. Este é um excelente produto que pode contribuir muito para o desempenho da sua máquina.")
-
-                            if 'KIT' in df_maquina.columns:
-                                kit_do_item = itens_filtrados['KIT'].values[0]
-                                itens_do_mesmo_kit = df_maquina[df_maquina['KIT']
-                                                                == kit_do_item]
-                                if not itens_do_mesmo_kit.empty:
-                                    st.write("Aqui estão outros itens que fazem parte do mesmo kit e que podem ser interessantes para você:")
-                                    st.dataframe(itens_do_mesmo_kit,
-                                                 use_container_width=True)
-                                    st.write("Oferecer um pacote completo desses itens pode garantir que sua máquina funcione perfeitamente por mais tempo. Podemos prosseguir com um orçamento?")
-                        else:
-                            st.write("Nenhum item encontrado com esse nome.")
-                else:
-                    st.warning(f"A coluna '{coluna_nome}' não foi encontrada na tabela da máquina selecionada.")
+                # Botão para carregar a imagem da máquina
+                if st.button("Mostrar Imagem da Máquina"):
+                    carregar_imagem_maquina(maquina_selecionada)
 
             except Exception as e:
                 st.error(f"Erro ao carregar os dados da máquina: {e}")
 
-    # Opção 2: Máquinas
-    elif menu_option == "Máquinas":
-        st.title("Máquinas")
+# Opção 3: Marcas
+elif menu_option == "Marcas":
+    st.title("Marcas")
+    marcas = ["Marca A", "Marca B", "Marca C"]
+    marca_selecionada = st.selectbox("Selecione a Marca:", marcas)
+    if marca_selecionada:
+        st.write(f"Informações detalhadas sobre a {marca_selecionada}.")
+        st.write("""
+        **Histórico:**  
+        A [Marca Selecionada] é uma das líderes no mercado, conhecida pela sua qualidade e inovação.
 
-        if abas:
-            maquinas_filtradas = abas
-            maquina_selecionada = st.selectbox(
-                "Selecione a Máquina:", maquinas_filtradas)
+        **Produtos Principais:**  
+        - Produto 1
+        - Produto 2
+        - Produto 3
 
-            if maquina_selecionada:
-                try:
-                    df_maquina = pd.read_excel(
-                        xls, sheet_name=maquina_selecionada)
+        **Diferenciais:**  
+        - Alta durabilidade
+        - Suporte técnico especializado
+        - Rede de distribuição abrangente
+        """)
 
-                    df_maquina = df_maquina.dropna(
-                        how='all').loc[:, ~df_maquina.columns.str.contains('^Unnamed')]
-
-                    st.title(f"Dados da Máquina: {maquina_selecionada}")
-                    st.dataframe(df_maquina, use_container_width=True)
-
-                    # Botão para carregar a imagem da máquina
-                    if st.button("Mostrar Imagem da Máquina"):
-                        carregar_imagem_maquina(maquina_selecionada)
-
-                except Exception as e:
-                    st.error(f"Erro ao carregar os dados da máquina: {e}")
-
-    # Opção 3: Marcas
-    elif menu_option == "Marcas":
-        st.title("Marcas")
-        marcas = ["Marca A", "Marca B", "Marca C"]
-        marca_selecionada = st.selectbox("Selecione a Marca:", marcas)
-        if marca_selecionada:
-            st.write(f"Informações detalhadas sobre a {marca_selecionada}.")
-            st.write("""
-            **Histórico:**  
-            A [Marca Selecionada] é uma das líderes no mercado, conhecida pela sua qualidade e inovação.
-
-            **Produtos Principais:**  
-            - Produto 1
-            - Produto 2
-            - Produto 3
-
-            **Diferenciais:**  
-            - Alta durabilidade
-            - Suporte técnico especializado
-            - Rede de distribuição abrangente
-            """)
-
-       # Opção 4: Venda mais (vídeos do YouTube)
+# Opção 4: Venda mais (vídeos do YouTube)
 elif menu_option == "Venda mais":
     st.title("Venda Mais - Dicas e Estratégias")
 
@@ -206,7 +171,6 @@ elif menu_option == "Venda mais":
     st.video("https://www.youtube.com/watch?v=TOp6oYruEg4")
     st.video("https://www.youtube.com/watch?v=Kh2m6prJHEU")
     st.video("https://www.youtube.com/watch?v=kaoGiCkVUHI")
-    st.video("https://www.youtube.com/watch?v=2J3eXja_D1U")
 
 # Opção 5: Segmentos
 elif menu_option == "Segmentos":
